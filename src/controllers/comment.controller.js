76ffc5f950bd1dcf8240fa3e71de42d84a049e6f
Abdullah-142 <abdulah14200@gmail.com>
2024-03-23 05:36:26 -0700

    Refactor comment and tweet controllers

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose"
 import { Comment } from "../models/comment.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/apiResponse.js"
@@ -9,7 +8,27 @@ const getVideoComments = asyncHandler(async (req, res) => {
   const { videoId } = req.params
   const { page = 1, limit = 10 } = req.query
 
+  if (!videoId) {
+    throw new ApiError(400, "Invalid video ID")
+  }
+
+  const video = await Comment.findById(videoId)
+
+  if (!video) {
+    throw new ApiError(404, "Video not found")
+  }
+
+  const pageNumber = parseInt(page)
+  const limitOfComments = parseInt(limit)
+
+  const skip = (pageNumber - 1) * limitOfComments
+  const pageSize = limitOfComments;
+
+
+
+
 })
+
 
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
@@ -31,15 +50,62 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  const { commentId } = req.params
+  const { content } = req.body
+  const { _id } = req.user
+
+  if (!content.trim()) {
+    throw new ApiError(400, "Comment content is required")
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found")
+  }
+
+  if (comment.owner.toString() !== _id.toString()) {
+    throw new ApiError(401, "You are not authorized to update this comment")
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(commentId, {
+    $set: {
+      content
+    }
+  }, {
+    new: true
+  })
+
+  if (!updatedComment) {
+    throw new ApiError(500, "Failed to update comment")
+  }
+
+  res.status(200).json(new ApiResponse(200, { updatedComment }, "Comment updated successfully"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-  // TODO: delete a comment
+  const { commentId } = req.params
+  const { _id } = req.user
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new ApiError(404, "Comment not found")
+  }
+
+  if (comment.owner.toString() !== _id.toString()) {
+    throw new ApiError(401, "You are not authorized to update this comment")
+  }
+
+  const deleteComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deleteComment) {
+    throw new ApiError(404, "Comment not found")
+  }
+
+  res.status(200).json(new ApiResponse(200, {}, "Comment deleted successfully"))
 })
 
 export {
-  getVideoComments,
-  addComment,
-  updateComment,
-  deleteComment
+  addComment, deleteComment, getVideoComments, updateComment
 }
--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -47,37 +47,62 @@ const getUserTweets = asyncHandler(async (req, res) => {
 const updateTweet = asyncHandler(async (req, res) => {
   //TODO: update tweet
   const { tweetId } = req.params
+  const { _id } = req.user
 
   if (!tweetId) {
     throw new ApiError(400, "Tweet ID is required")
   }
 
-  const tweet = await Tweet.findByIdAndUpdate(tweetId, {
+  const tweet = await Tweet.findById(tweetId);
+
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found")
+  }
+
+  if (tweet.owner.toString() !== _id.toString()) {
+    throw new ApiError(401, "You are not authorized to update this tweet")
+  }
+
+  const updateTweet = await Tweet.findByIdAndUpdate(tweetId, {
     $set: {
       content: req.body.content
     }
   }, {
     new: true
   })
-  if (!tweet) {
+  if (!updateTweet) {
     throw new ApiError(404, "Tweet not found")
   }
 
-  res.status(200).json(new ApiResponse(200, { tweet }, "Tweet updated successfully"))
+  res.status(200).json(new ApiResponse(200, {}, "Tweet updated successfully"))
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
-
   const { tweetId } = req.params
 
   if (!tweetId) {
     throw new ApiError(400, "Tweet ID is required")
   }
 
-  const tweet = await Tweet.findByIdAndDelete(tweetId)
+
+  if (!tweetId) {
+    throw new ApiError(400, "Tweet ID is required")
+  }
+
+  const tweet = await Tweet.findById(tweetId);
 
   if (!tweet) {
+    throw new ApiError(404, "Tweet not found")
+  }
+
+  if (tweet.owner.toString() !== _id.toString()) {
+    throw new ApiError(401, "You are not authorized to update this tweet")
+  }
+
+  const deleteTweet = await Tweet.findByIdAndDelete(tweetId)
+
+  if (!deleteTweet) {
     throw new ApiError(404, "Tweet not found")
   }
 
